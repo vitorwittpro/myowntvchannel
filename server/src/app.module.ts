7fa46c3e8a522d334efc11f31c88d61565da5c9c
Vitor Hugo Witt <vitorwitt.pro@gmail.com>
2025-01-20 20:31:41 -0300

    feat: :wrench: configure database connection with typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideoModule } from './modules/video.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [VideoModule],
+  imports: [
+    VideoModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'postgres',
+      entities: [],
+      // Warning
+      synchronize: true, // Setting synchronize: true shouldn't be used in production - otherwise you can lose production data.
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
